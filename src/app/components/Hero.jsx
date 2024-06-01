3ddc32dd92cf36db85a2a5c269f598e4fc61cbf0
Mahak <mahakgarg8002@gmail.com>
2024-06-01 19:57:45 +0530

    Added Resume

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -16,7 +16,9 @@ const Hero = () => {
 
         <div className='col-span-7 place-self-center text-center sm:text-right justify-self-start'>
           <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl lg:leading-normal font-extrabold'>
-            <div className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'> Hello, I am a</div>  
+            <div className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'> 
+              Hello, I am a
+            </div>  
             <TypeAnimation
               sequence={[
                 'Web Developer',
@@ -40,7 +42,7 @@ const Hero = () => {
               Hire Me
             </Link>
 
-            <Link href='/' className='px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3'>
+            <Link href='./Mahak_Garg_Resume.pdf' rel='noreferrer noopener' target='_blank' className='px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3'>
               <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>
                 Download CV
               </span>
