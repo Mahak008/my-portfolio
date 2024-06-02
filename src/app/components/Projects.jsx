84cb8ae99b01dfa7e3632d214f111f9dd1ee159a
Mahak <mahakgarg8002@gmail.com>
2024-06-02 13:52:12 +0530

    Project Section

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
 
 const projectsData = [
   {
@@ -59,13 +61,31 @@ const projectsData = [
 ];
 
 const Projects = () => {
+  const [tag, setTag] = useState("All");
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
   return (
     <section id="pro">
       <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
         My Projects
       </h2>
-      <div>
-        {projectsData.map((project) => (
+
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag onClick={handleTagChange} name="All" isSelected={tag === "All"} />
+        <ProjectTag onClick={handleTagChange} name="MERN" isSelected={tag === "MERN"} />
+        <ProjectTag onClick={handleTagChange} name="Java" isSelected={tag === "Java"} />
+        <ProjectTag onClick={handleTagChange} name="Python" isSelected={tag === "Python"} />
+      </div>
+
+      <div className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project) => (
           <ProjectCard
             key={project.id}
             title={project.title}
