a86fad7666ccbb3815bda54cf081c94e2cb4084a
Mahak <mahakgarg8002@gmail.com>
2024-06-02 12:21:41 +0530

    Project Card

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -1,35 +1,35 @@
-"use client"
-import Image from 'next/image'
-import React, { useState, useTransition } from 'react'
-import TabButton from './TabButton';
+"use client";
+import Image from "next/image";
+import React, { useState, useTransition } from "react";
+import TabButton from "./TabButton";
 
 const TAB_DATA = [
-    {
-        title: "Skills",
-        id: "skills",
-        content: (
-            <ul className='list-disc pl-2'>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>Bootstrap</li>
-                <li>Tailwind</li>
-                <li>Javascript</li>
-                <li>React</li>
-                <li>MySQL</li>
-            </ul>
-        ),
-    },
-    {
-        title: "Certifications",
-        id: "cert",
-        content: (
-            <ul className='list-disc pl-2'>
-                <li>Web Development Training by Internshala.</li>
-                <li>Responsive Web Design Developer Certification by freeCodeCamp.</li>
-                <li>HTML, CSS and Javascript for Web Developers by Coursera.</li>
-            </ul>
-        ),
-    },
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Bootstrap</li>
+        <li>Tailwind</li>
+        <li>Javascript</li>
+        <li>React</li>
+        <li>MySQL</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "cert",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Web Development Training by Internshala.</li>
+        <li>Responsive Web Design Developer Certification by freeCodeCamp.</li>
+        <li>HTML, CSS and Javascript for Web Developers by Coursera.</li>
+      </ul>
+    ),
+  },
 ];
 
 const About = () => {
@@ -38,39 +38,55 @@ const About = () => {
 
   const handleTabChange = (id) => {
     startTransition(() => {
-        setTab(id);
+      setTab(id);
     });
   };
-  
+
   return (
-    <section className='text-white' id='about'>
-      <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
-        <div className='mt-4 md:mt-0 text-left flex flex-col h-full mb-2'>
-            <h2 className='text-4xl font-bold text-white mb-4'>About Me</h2>
-            
-            <p className='text-base lg:text-lg text-justify'>
-                As a passionate Frontend Developer, I excel in crafting intuitive and engaging user experiences. I transform ideas into responsive and visually appealing web applications. With a keen eye for detail, I strive to create seamless digital experiences that delight users and drive business success. My goal is to blend creativity and functionality in every project.
-            </p>
-            
-            <div className='flex flex-row justify-start mt-8'>
-                <TabButton selectTab={() => handleTabChange("skills")} active={tab === "skills"}>
-                    {" "}
-                    Skills{" "}
-                </TabButton>
-                <TabButton selectTab={() => handleTabChange("cert")} active={tab === "cert"}>
-                    {" "}
-                    Certifications{" "}
-                </TabButton>
-            </div>
-            
-            <div className='mt-8'>
-                {TAB_DATA.find((t) => t.id === tab).content}
-            </div>
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full mb-2">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+
+          <p className="text-base lg:text-lg text-justify">
+            As a passionate Frontend Developer, I excel in crafting intuitive
+            and engaging user experiences. I transform ideas into responsive and
+            visually appealing web applications. With a keen eye for detail, I
+            strive to create seamless digital experiences that delight users and
+            drive business success. My goal is to blend creativity and
+            functionality in every project.
+          </p>
+
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("cert")}
+              active={tab === "cert"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
         </div>
-        <Image src='/images/about-image.png' width={500} height={500} alt='About' />
+        <Image
+          src="/images/about-image.png"
+          width={500}
+          height={500}
+          alt="About"
+        />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -1,18 +1,83 @@
-import React from 'react'
+import React from "react";
+import ProjectCard from "./ProjectCard";
 
-const Projects = ({ imgURL, title, desc }) => {
+const projectsData = [
+  {
+    id: 1,
+    title: "Portfolio Website",
+    description: "Project 1 description",
+    image: "/images/projects/P1.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 2,
+    title: "Hungry Box",
+    description: "Project 2 description",
+    image: "/images/projects/P1.png",
+    tag: ["All", "MERN"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 3,
+    title: "University Complaint Management System",
+    description: "Project 3 description",
+    image: "/images/projects/P1.png",
+    tag: ["All", "Java"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 4,
+    title: "Pharmacy Management System",
+    description: "Project 4 description",
+    image: "/images/projects/P1.png",
+    tag: ["All", "Python"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 5,
+    title: "Smart Calculator",
+    description: "Authentication and CRUD operations",
+    image: "/images/projects/P1.png",
+    tag: ["All", "Python"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 6,
+    title: "Project Management System",
+    description: "Project 5 description",
+    image: "/images/projects/P1.png",
+    tag: ["All", "MERN"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+];
+
+const Projects = () => {
   return (
-    <div>
-      <div style={{background: `url(${imgURL})`, backgroundSize: "cover"}}
-      className='h-52 md:h-72'>
-        <div className='text-white'>
-            <h5>{title}</h5>
-            <p>{desc}</p>
-        </div>
+    <section id="pro">
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        My Projects
+      </h2>
+      <div>
+        {projectsData.map((project) => (
+          <ProjectCard
+            key={project.id}
+            title={project.title}
+            desc={project.description}
+            imgURL={project.image}
+            gitUrl={project.gitUrl}
+            previewUrl={project.previewUrl}
+          />
+        ))}
       </div>
-      <h2>My Projects</h2>
-    </div>
-  )
-}
+    </section>
+  );
+};
 
-export default Projects
+export default Projects;
