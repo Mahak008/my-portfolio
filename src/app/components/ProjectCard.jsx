b6b2b6c764856470d2806871f271c30623f9bd24
Mahak <mahakgarg8002@gmail.com>
2024-06-11 22:08:03 +0530

    Project

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -5,10 +5,10 @@ import Link from "next/link";
 
 const ProjectCard = ({ imgURL, title, desc, gitUrl, previewUrl }) => {
   return (
-    <div>
+    <div className="rounded-xl overflow-hidden shadow-lg bg-[#181818]">
       <div
-        style={{ background: `url(${imgURL})`, backgroundSize: "cover" }}
-        className="h-52 md:h-72 rounded-t-xl relative group"
+        style={{ background: `url(${imgURL})`, backgroundSize: "cover", backgroundPosition: "center" }}
+        className="h-52 md:h-72 relative group"
       >
         <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500">
           <Link
@@ -25,11 +25,11 @@ const ProjectCard = ({ imgURL, title, desc, gitUrl, previewUrl }) => {
             <IoEyeOutline className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white" />
           </Link>
         </div>
+      </div>
 
-        <div className="text-white rounded-b-xl mt-3 bg-[#181818] py-6 px-4">
-          <h5 className="text-xl font-semibold mb-2">{title}</h5>
-          <p className="text-[#ADB7BE]">{desc}</p>
-        </div>
+      <div className="text-white py-6 px-4 bg-[#181818]">
+        <h5 className="text-xl font-semibold mb-2">{title}</h5>
+        <p className="text-[#ADB7BE]">{desc}</p>
       </div>
     </div>
   );
