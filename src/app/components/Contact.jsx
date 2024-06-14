8a8753764ed663a080ca549fb960a873b0a01740
Mahak <mahakgarg8002@gmail.com>
2024-06-14 23:15:02 +0530

    Contact Section

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -1,11 +1,16 @@
+"use client";
 import React from "react";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import Link from "next/link";
 
 const Contact = () => {
   return (
-    <section className="grid md:grid-cols-2 my-12 py-24 gap-4" id="con">
-      <div>
+    <section
+      className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
+      id="con"
+    >
+      <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
+      <div className="z-10">
         <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
           {" "}
@@ -15,26 +20,18 @@ const Contact = () => {
         </p>
 
         <div className="socials flex flex-row gap-2">
-          <Link
-            href="https://www.github.com/Mahak008"
-            target="_blank"
-            alt="Github"
-          >
+          <Link href="https://www.github.com/Mahak008" target="_blank">
             <FaGithub />
           </Link>
-          <Link
-            href="https://www.linkedin.com/in/mahakgarg"
-            target="_blank"
-            alt="Linkedin"
-          >
+          <Link href="https://www.linkedin.com/in/mahakgarg" target="_blank">
             <FaLinkedin />
           </Link>
         </div>
       </div>
 
       <div>
-        <form className="flex flex-col">
-          <div className="mb-6">
+        <form className="flex flex-col w-full">
+          <div className="mb-6 w-full">
             <label
               htmlFor="email"
               className="text-white block mb-2 text-sm font-medium"
@@ -43,15 +40,16 @@ const Contact = () => {
             </label>
 
             <input
+              name="email"
               type="email"
               id="email"
               placeholder="Your Email"
-              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block-w-full p-2.5"
+              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg w-full p-2.5"
               required
             />
           </div>
 
-          <div className="mb-6">
+          <div className="mb-6 w-full">
             <label
               htmlFor="subject"
               className="text-white block mb-2 text-sm font-medium"
@@ -60,22 +58,37 @@ const Contact = () => {
             </label>
 
             <input
+              name="subject"
               type="text"
               id="subject"
               placeholder="Subject of Email"
-              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block-w-full p-2.5"
+              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg w-full p-2.5"
               required
             />
           </div>
 
-          <div className="mb-6">
+          <div className="mb-6 w-full">
             <label
               htmlFor="message"
               className="text-white block text-sm mb-2 font-medium"
             >
               Message
             </label>
-            <textarea name="message" id="message" placeholder="Your Message" className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block-w-full p-2.5" />
+            <textarea
+              name="message"
+              id="message"
+              placeholder="Your Message"
+              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg w-full p-2.5"
+            />
+          </div>
+
+          <div className="w-full">
+            <button
+              type="submit"
+              className="bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
+            >
+              Send Message
+            </button>
           </div>
         </form>
       </div>
