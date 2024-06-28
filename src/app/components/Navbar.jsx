c4a04d26f81068ec714b7a6b3e5c25c4074d6d1c
Mahak <mahakgarg8002@gmail.com>
2024-06-28 12:33:41 +0530

    Navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -2,10 +2,13 @@
 import Link from "next/link";
 import React, { useState } from "react";
 import NavLink from "./NavLink";
-import { HiMenuAlt2 } from "react-icons/hi";
-import { RxCross2 } from "react-icons/rx";
 import MenuOverlay from "./MenuOverlay";
 import { motion } from "framer-motion";
+import { HiMenuAlt2 } from "react-icons/hi";
+import { RxCross2 } from "react-icons/rx";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { SiLeetcode, SiGeeksforgeeks } from "react-icons/si";
+import { MdEmail } from "react-icons/md";
 
 const navLinks = [
   {
@@ -50,6 +53,7 @@ const Navbar = () => {
             </button>
           )}
         </div>
+
         <div className="menu hidden md:block md:w-auto" id="navbar">
           <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
             {navLinks.map((link, index) => (
@@ -60,15 +64,33 @@ const Navbar = () => {
           </ul>
         </div>
 
-        <div className="flex items-center justify-center mt-2">
+        <div className="flex items-center justify-center mt-2 absolute left-[50%] top-2 translate-x-[-50%]">
           <MotionLink
             href={"/"}
             className="w-16 h-16 bg-black text-white flex items-center justify-center rounded-full text-2xl font-bold"
-            whileHover={{ scale: 1.2 }}
+            whileHover={{
+              backgroundColor: [
+                "#121212",
+                "rgba(131, 58,180,1)",
+                "rgba(253,29,29,1)",
+                "rgba(252,176,69,1)",
+                "rgba(131,58,180,1)",
+                "#121212",
+              ],
+              transition: { duration: 1, repeat: Infinity },
+            }}
           >
             MG
           </MotionLink>
         </div>
+
+        <nav>
+        <Link href="" target="_blank"><MdEmail /></Link>
+        <Link href="https://www.github.com/Mahak008" target="_blank"><FaGithub /></Link>
+        <Link href="https://www.geeksforgeeks.org/user/mahak_garg/" target="_blank"><SiGeeksforgeeks /></Link>
+        <Link href="https://leetcode.com/u/Mahak80/" target="_blank"><SiLeetcode /></Link>
+          <Link href="https://www.linkedin.com/in/mahakgarg" target="_blank"><FaLinkedin /></Link>
+        </nav>
       </div>
 
       {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
